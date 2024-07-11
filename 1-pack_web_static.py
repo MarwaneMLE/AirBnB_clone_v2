1d4c2a774ef6ebe7b26076b7aad3ef4c69d51e0b
Marwane <khmarwane10@gmail.com>
2024-07-11 08:36:41 -0700

    Compress static files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents of the web_static folder
-of your AirBnB Clone repo, using the function do_pack.
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
 
 from datetime import datetime
 from fabric.api import *
+
 
 def do_pack():
     """
@@ -20,4 +21,3 @@ def do_pack():
         return archive
     else:
         return None
-
